66ed8d6f451e4dda9ef25a658e5f64e05fc5e186
Yassir Arafat <roni.yassir@gmail.com>
2017-01-23 17:57:03 +0600

    Insert Update Delete Database

--- app/src/main/java/app/main/com/databasehelper/DatabaseHelper.java
+++ app/src/main/java/app/main/com/databasehelper/DatabaseHelper.java
@@ -2,6 +2,7 @@ package app.main.com.databasehelper;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 /**
@@ -44,5 +45,26 @@ public class DatabaseHelper extends SQLiteOpenHelper{
         }
         else
             return true;
+    }
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+
+    public boolean updateData(String id,String name,String surname,String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,surname);
+        contentValues.put(COL_4,marks);
+        db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
+        return true;
+    }
+
+    public Integer deleteData (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?",new String[] {id});
     }
 }
